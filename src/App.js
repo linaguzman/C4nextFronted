7961463d7507f31eefb28f259d24b47f9e647e4f
Gweskaine <dianacris.tobon.m@gmail.com>
2021-12-15 22:55:41 -0500

    proyecto tailwin con paginas

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route ,Link, Routes} from "react-router-dom";
+import "./styles/App.css" 
+import Page1 from"./components/page1" 
+import Page2 from"./components/page2" 
+import Page3 from"./components/page3" 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-      </header>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<h1>Home Page</h1>} />
+          <Route exact path="page1" element={<Page1 />} />
+          <Route exact path="page2" element={<Page2 />} />
+          <Route exact path="page3" element={<Page3 />} />
+        </Routes>
+        <div className="list">
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="page1">Page 1</Link></li>
+            <li><Link to="page2">Page 2</Link></li>
+            <li><Link to="page3">Page 3</Link></li>
+          </ul>
+        </div>
+      </Router>
     </div>
   );
 }
