151df3fd456cd9da7095690a23932c77d6747ec7
Gweskaine <dianacris.tobon.m@gmail.com>
2021-12-18 00:02:58 -0500

    agrego todos los imports para las paginas en el router

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route ,Link, Routes} from "react-router-dom";
+import { UserContext } from './context/userContext';
+import {   ApolloProvider,  ApolloClient,  createHttpLink,  InMemoryCache,} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import './styles/globals.css';
 import './styles/tabla.css';
 
@@ -12,42 +16,112 @@ import EditarUsuario from './pages/usuarios/editar';
 import AuthLayout from './layouts/AuthLayout';
 import Register from './pages/auth/register.jsx';
 import Login from './pages/auth/login';
-//import { AuthContext } from 'context/authContext';
+import { AuthContext } from 'context/authContext';
 import IndexProyectos from './pages/proyectos/Index';
-//import jwt_decode from 'jwt-decode';
+import jwt_decode from 'jwt-decode';
 import NuevoProyecto from './pages/proyectos/NuevoProyecto';
 import IndexInscripciones from './pages/inscripciones';
 import Profile from './pages/profile';
+import IndexAvance from 'pages/avances';
 
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = JSON.parse(localStorage.getItem('token'));
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const httpLink = createHttpLink({
+  // uri: 'https://git.heroku.com/c4nextfronted.git',
+  uri: 'http://localhost:4000/graphql',
+});
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: authLink.concat(httpLink),
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = JSON.parse(localStorage.getItem('token'));
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: authLink.concat(httpLink),
+});
 
 function App() {
+  const [userData, setUserData] = useState({});
+  const [authToken, setAuthToken] = useState('');
+
+  const setToken = (token) => {
+    setAuthToken(token);
+    if (token) {
+      localStorage.setItem('token', JSON.stringify(token));
+    } else {
+      localStorage.removeItem('token');
+    }
+  };
+
+  useEffect(() => {
+    if (authToken) {
+      const decoded = jwt_decode(authToken);
+      setUserData({
+        _id: decoded._id,
+        nombre: decoded.nombre,
+        apellido: decoded.apellido,
+        identificacion: decoded.identificacion,
+        correo: decoded.correo,
+        rol: decoded.rol,
+        foto: decoded.foto,
+      });
+    }
+  }, [authToken]);
+
   return (
-    <div className="App">
-      <Router>
-        <Routes>
-        <Route path='/' element={<PrivateLayout />}>
+    <ApolloProvider client={client}>
+      <AuthContext.Provider value={{ authToken, setAuthToken, setToken }}>
+        <UserContext.Provider value={{ userData, setUserData }}>
+          <BrowserRouter>
+            <Routes>
+              <Route path='/' element={<PrivateLayout />}>
                 <Route path='' element={<Index />} />
-                <Route path='./usuarios' element={<IndexUsuarios />} />
+                <Route path='/usuarios' element={<IndexUsuarios />} />
                 <Route
                   path='/usuarios/editar/:_id'
                   element={<EditarUsuario />}
                 />
-                <Route path='./pages/proyectos' element={<IndexProyectos />} />
-                <Route path='./pages/proyectos/nuevo' element={<NuevoProyecto />} />
-                <Route path='./pages/inscripciones' element={<IndexInscripciones />} />
-                <Route path='./pages/profile' element={<Profile />} />
-                <Route path='./pages/page2' element={<Page2 />} />
-                <Route path='./pages/category1' element={<IndexCategory1 />} />
-                <Route path='./pages/category1/page1' element={<Category1 />} />
+                <Route path='/proyectos' element={<IndexProyectos />} />
+                <Route path='/proyectos/nuevo' element={<NuevoProyecto />} />
+                <Route path='/inscripciones' element={<IndexInscripciones />} />
+                <Route path='/avances/:projectid' element={<IndexAvance />} />
+                <Route path='/perfil' element={<Profile />} />
+                <Route path='page2' element={<Page2 />} />
+                <Route path='category1' element={<IndexCategory1 />} />
+                <Route path='category1/page1' element={<Category1 />} />
               </Route>
-              <Route path='./auth' element={<AuthLayout />}>
-                <Route path='./register' element={<Register />} />
-                <Route path='./login' element={<Login />} />
+              <Route path='/auth' element={<AuthLayout />}>
+                <Route path='register' element={<Register />} />
+                <Route path='login' element={<Login />} />
               </Route>
-        </Routes>
-        
-      </Router>
-    </div>
+            </Routes>
+          </BrowserRouter>
+        </UserContext.Provider>
+      </AuthContext.Provider>
+    </ApolloProvider>
   );
 }
 
